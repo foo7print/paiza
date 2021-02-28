b8e38bdf47fe4045053e9007cf4958cd73779831
foo7print <foo7print@users.noreply.github.com>
2021-02-28 20:09:09 +0900

    0005_fizzbuzz.py

---
+++ py/0005_fizzbuzz.py
@@ -0,0 +1,11 @@
+input_line = int(input())
+for i in range(1, input_line + 1):
+    if i % 3 == 0 and i % 5 == 0:
+        print("Fizz Buzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+
