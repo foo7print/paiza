a82c66105f958676604c7176becbfe78c3d756c5
foo7print <foo7print@users.noreply.github.com>
2021-03-13 13:54:06 +0900

    0003_min.swift

---
+++ swift/0003_min.swift
@@ -0,0 +1,8 @@
+let n_1 = Int(readLine()!)!
+let n_2 = Int(readLine()!)!
+let n_3 = Int(readLine()!)!
+let n_4 = Int(readLine()!)!
+let n_5 = Int(readLine()!)!
+let a: [Int] = [n_1, n_2, n_3, n_4, n_5]
+print(a.min()!)
+
