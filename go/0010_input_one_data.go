a18cc81584d12811a0f1eb8e08c06dc2e4b34513
foo7print <foo7print@users.noreply.github.com>
2021-04-24 11:06:47 +0900

    0010_input_one_data.go

--- go/0010_input_one_data.go
+++ go/0010_input_one_data.go
@@ -1,10 +1,13 @@
 package main
 
 import "fmt"
+import "bufio"
+import "os"
 
-func main(){
-    var s string
-    fmt.Scan(&s)
-    fmt.Println(s)
+func main() {
+    scanner := bufio.NewScanner(os.Stdin)
+    scanner.Scan()
+    t := scanner.Text()
+    fmt.Println(t)
 }
 
