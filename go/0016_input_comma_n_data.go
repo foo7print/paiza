b5b2daed50fcc4caeb8d3d5df68b9d302cbc8611
foo7print <foo7print@users.noreply.github.com>
2021-04-29 12:04:47 +0900

    0016_input_comma_n_data.go

---
+++ go/0016_input_comma_n_data.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "fmt"
+    "bufio"
+    "os"
+    "strconv"
+    "strings"
+)
+
+func main() {
+    sc := bufio.NewScanner(os.Stdin)
+    sc.Scan()
+    var n, _ = strconv.Atoi(sc.Text())
+    sc.Scan()
+    list := strings.Split(sc.Text(), ",")
+    for i := 0; i < n; i++ {
+        fmt.Println(list[i])
+    }
+}
+
