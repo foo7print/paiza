0ce6f699b6d8cf2f4bcb8eb9484497528eb8578a
foo7print <foo7print@users.noreply.github.com>
2021-05-30 18:28:03 +0900

    0002_addition.js

---
+++ 0002_addition.js
@@ -0,0 +1,14 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  var a = lines[0].split(' ').map(Number);
+  console.log(a[0] + a[1]);
+});
